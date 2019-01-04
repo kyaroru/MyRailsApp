3643fd1ecddf928a5e47a32a5cb5352ab8b4cdf1
Carol <kyaroru@outlook.com>
2019-01-04 16:14:39 +0800

    Update apps

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'cover#index'
-  get 'home', to: 'home#index'
+  get 'profile', to: 'profile#index'
   get 'about', to: 'about#index'
   get 'apps', to: 'apps#index'
 end
